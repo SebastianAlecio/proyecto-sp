80b0a70343b15a785855890d209f978b7d0ca99c
Alessandro Alecio <62570049+SebastianAlecio@users.noreply.github.com>
2025-08-19 15:49:20 -0600

    Updated 20250819014618_violet_paper.sql

--- supabase/migrations/20250819014618_violet_paper.sql
+++ supabase/migrations/20250819014618_violet_paper.sql
@@ -1,4 +1,4 @@
--- Update the Ñ character image URL
+-- Update Ñ character image URL
 UPDATE letters 
 SET image_url = 'https://aipphlqzhtkwmlnkkrgt.supabase.co/storage/v1/object/public/sign-images/LSG_ENE'
 WHERE character = 'Ñ';
